f3d1c516c94097e80e13ed9e240ec18f0a8f8c70
gmottola00 <gianmarco.mottola@icloud.com>
2025-09-17 18:35:09 +0200

    ticket  schemas

--- backend/app/schemas/ticket.py
+++ backend/app/schemas/ticket.py
@@ -1,18 +1,21 @@
-from pydantic import BaseModel
-from uuid import UUID
 from datetime import datetime
+from uuid import UUID
+from pydantic import BaseModel
 
 class TicketBase(BaseModel):
     pool_id: UUID
     user_id: UUID
     purchase_id: UUID
-    ticket_num: int
 
 class TicketCreate(TicketBase):
     pass
 
+class TicketPurchaseRequest(BaseModel):
+    purchase_id: UUID
+
 class Ticket(TicketBase):
     ticket_id: int
+    ticket_num: int
     created_at: datetime
 
     class Config:
