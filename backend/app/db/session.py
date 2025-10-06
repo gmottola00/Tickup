17b8099ff3a8fc2147945686a4403dbad0b38cf2
Gianmarco Mottola <gianmarco.mottola@it.ey.com>
2025-10-06 20:11:43 +0200

    update the engine and the async session for the db

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -2,8 +2,19 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, Asyn
 from app.core.config import settings
 from typing import AsyncGenerator
 
-engine = create_async_engine(settings.DATABASE_URL, echo=True)
-async_session = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+engine = create_async_engine(
+    settings.DATABASE_URL,
+    echo=True,
+    connect_args={
+        "statement_cache_size": 0,
+        "prepared_statement_cache_size": 0,
+    },
+)
+async_session = async_sessionmaker(
+    engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
 
 async def get_db() -> AsyncGenerator[AsyncSession, None]:
     async with async_session() as session:
