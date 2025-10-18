12070302e43eb22194251ff9073d216df22714ae
gmottola00 <gianmarco.mottola@icloud.com>
2025-10-18 02:10:36 +0200

    Pixel Adventure v.1.0

--- frontend/lib/presentation/pages/games/game_launcher.dart
+++ frontend/lib/presentation/pages/games/game_launcher.dart
@@ -6,11 +6,12 @@ import 'package:tickup/presentation/routing/app_route.dart';
 class GameLauncher extends StatelessWidget {
   const GameLauncher({Key? key}) : super(key: key);
 
-  static const List<Map<String, String>> _games = [
-    {'title': 'Stop Bar', 'route': '/games/stop_bar'},
-    {'title': 'Reflex Tap', 'route': '/games/reflex_tap'},
-    {'title': 'Space Invaders', 'route': '/games/space_invaders'},
-    {'title': 'Tanks (Unity)', 'route': AppRoute.unityTanks},
+  static final List<Map<String, String>> _games = [
+    const {'title': 'Stop Bar', 'route': '/games/stop_bar'},
+    const {'title': 'Reflex Tap', 'route': '/games/reflex_tap'},
+    const {'title': 'Space Invaders', 'route': '/games/space_invaders'},
+    {'title': 'Pixel Adventure', 'route': AppRoute.game('pixel_adventure')},
+    const {'title': 'Tanks (Unity)', 'route': AppRoute.unityTanks},
   ];
 
   @override
--- frontend/lib/presentation/pages/games/game_runner.dart
+++ frontend/lib/presentation/pages/games/game_runner.dart
@@ -1,16 +1,54 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class GameRunner extends StatelessWidget {
+import 'package:flame/flame.dart';
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+import 'package:tickup/pixel_adventure.dart';
+
+class GameRunner extends StatefulWidget {
   final String gameId;
   const GameRunner({Key? key, required this.gameId}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return _buildGameWidget(gameId);
+  State<GameRunner> createState() => _GameRunnerState();
+}
+
+class _GameRunnerState extends State<GameRunner> {
+  late final Widget _gameContent;
+
+  @override
+  void initState() {
+    super.initState();
+    _gameContent = _buildGameWidget(widget.gameId);
+    _enterGameMode();
   }
+
+  @override
+  void dispose() {
+    _exitGameMode();
+    super.dispose();
+  }
+
+  void _enterGameMode() {
+    unawaited(Flame.device.fullScreen());
+    unawaited(Flame.device.setLandscape());
+  }
+
+  void _exitGameMode() {
+    unawaited(Flame.device.restoreFullscreen());
+    unawaited(Flame.device.setPortrait());
+  }
+
+  @override
+  Widget build(BuildContext context) => _gameContent;
 
   Widget _buildGameWidget(String id) {
     switch (id) {
+      case 'pixel_adventure':
+        return Scaffold(
+          backgroundColor: const Color(0xFF211F30),
+          body: GameWidget(game: PixelAdventure()),
+        );
       // case 'space_invaders':
       //   return const SpaceInvadersPage();
       // case 'stop_bar':
